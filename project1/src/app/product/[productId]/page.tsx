7c321b2be211db54b7baa839d54c54a95ac3eccb
Shreyas <sonureigns007@gmail.com>
2024-02-03 17:09:18 +0530

    nested routing

---
+++ project1/src/app/product/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default function subProduct({ params }) {
+  return <h1>Product {params.productId}</h1>;
+}
---
+++ project1/src/app/product/page.tsx
@@ -0,0 +1,3 @@
+export default function product() {
+  return <h1>All product</h1>;
+}
