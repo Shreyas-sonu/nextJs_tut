ee8aa6c6de6444ee273105a8c6cf1bb1cc3b5363
Shreyas <sonureigns007@gmail.com>
2024-02-03 18:40:35 +0530

    catch all segments

---
+++ project1/src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,15 @@
+export default function docs({ params }: { params: { slug: string[] } }) {
+    console.log(params)
+  if (params?.slug?.length === 2) {
+    return (
+      <h1>
+        docs you are in {params.slug[0]} of the {params.slug[1]}
+      </h1>
+    );
+  } else if (params?.slug?.length === 1) {
+    return <h1>docs you are in {params.slug[0]} of the doc home</h1>;
+  } else if (params?.slug?.length > 2) {
+    return <h1>docs you are away from us {params.slug.join("")}</h1>;
+  }
+  return <h1>docs Home</h1>;
+}
